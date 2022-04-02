86eaf40e7472d03fd861a6a3f418b583dbf18bda
caalpeva <boolbee01@gmail.com>
2022-04-02 19:55:25 +0200

    Add more files

---
+++ src/main/java/team/boolbee/poc_calculator/PocCalculatorApplication.java
@@ -0,0 +1,13 @@
+package team.boolbee.poc_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/boolbee/poc_calculator/PocCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package team.boolbee.poc_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
