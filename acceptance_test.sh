fc8057f78b1a5ce95133e53baa7aed0ba63a542b
caalpeva <boolbee01@gmail.com>
2022-04-05 13:33:38 +0200

    Get ip address from container

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.Networks.IPAddress }}" calculator)
+CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" calculator)
 echo "IP address: ${CALCULATOR_IP}"
 CALCULATOR_PORT=8082
 #CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
