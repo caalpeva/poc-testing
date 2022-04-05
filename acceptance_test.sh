b8453d6070cba56f390b01e77d082fde5ef6d905
caalpeva <boolbee01@gmail.com>
2022-04-05 11:25:30 +0200

    Add port number

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CALCULATOR_PORT=$(docker-compose port poc-calculator 8080 | cut -d: -f2)
+echo "El numero de puerto es ${CALCULATOR_PORT}"
 test $(curl localhost:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
