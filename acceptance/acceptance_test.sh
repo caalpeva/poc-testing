e777ecdc84b40571ab1d4fd63751f42d59ece2ed
caalpeva <boolbee01@gmail.com>
2022-04-05 16:51:40 +0200

    Fix acceptance test stage

--- acceptance/acceptance_test.sh
+++ acceptance/acceptance_test.sh
@@ -2,4 +2,5 @@
 
 #CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" calculator)
 #CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
+sleep 10
 test $(curl calculator:8080/sum?a=1\&b=2) -eq 3
