f3284e0032b14ffc46ab437486e72c479bb1c75a
caalpeva <boolbee01@gmail.com>
2022-04-05 11:29:51 +0200

    Fix Jenkinsfile

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker-compose version
 CALCULATOR_PORT=$(docker-compose port poc-calculator 8080 | cut -d: -f2)
 echo "El numero de puerto es ${CALCULATOR_PORT}"
 test $(curl localhost:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
