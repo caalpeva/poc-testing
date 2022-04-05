d97298a110625f72db3dcf021ad391629f7db786
caalpeva <boolbee01@gmail.com>
2022-04-05 14:01:08 +0200

    Try to fix acceptance test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -5,4 +5,4 @@ echo "IP address: ${CALCULATOR_IP}"
 CALCULATOR_PORT=8082
 #CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
 echo "Port: ${CALCULATOR_PORT}"
-test $(curl ${CALCULATOR_IP}:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
+test $(curl localhost:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
