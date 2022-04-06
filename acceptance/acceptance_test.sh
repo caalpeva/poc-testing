88f8e364f71b14432d413fc80ea29ba799d1760e
caalpeva <boolbee01@gmail.com>
2022-04-06 11:13:55 +0200

    Activate acceptance test with cucumber

--- acceptance/acceptance_test.sh
+++ acceptance/acceptance_test.sh
@@ -3,5 +3,5 @@
 #CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" calculator)
 #CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
 sleep 10
-test $(curl calculator:8080/sum?a=1\&b=2) -eq 3
-#./gradlew acceptanceTest -Dcalculator.url=http://calculator:8080
+#test $(curl calculator:8080/sum?a=1\&b=2) -eq 3
+./gradlew acceptanceTest -Dcalculator.url=http://calculator:8080
