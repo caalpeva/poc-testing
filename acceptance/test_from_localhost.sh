a93c8df88bc839209155ccb5b360d2ddbc6b6953
caalpeva <kalpeva@gmail.com>
2022-04-11 20:48:48 +0200

    Add more test files

--- acceptance/test_from_localhost.sh
+++ acceptance/test_from_localhost.sh
@@ -2,3 +2,4 @@
 
 CALCULATOR_PORT=8080
 ./gradlew acceptanceTest -Dcalculator.url=http://localhost:${CALCULATOR_PORT}
+#test $(curl localhost:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
--- acceptance/test_from_machine_remote.sh
+++ acceptance/test_from_machine_remote.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sleep 10
-CALCULATOR_PORT=9090
+CALCULATOR_PORT=8080
 test $(curl poc-webapp-calculator:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
