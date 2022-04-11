75d5c140d40efdd1f6d96a95ed56ae6641ab379a
caalpeva <kalpeva@gmail.com>
2022-04-11 20:52:49 +0200

    Add network name to get ip address

--- test_from_machine_jenkins.sh
+++ test_from_machine_jenkins.sh
@@ -2,6 +2,6 @@
 
 sleep 10
 CALCULATOR_PORT=8080
-CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" calculator)
+CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.Networks.acceptance_default.IPAddress }}" calculator)
 ./gradlew acceptanceTest -Dcalculator.url=http://${CALCULATOR_IP}:${CALCULATOR_PORT}
 #test $(curl ${CALCULATOR_IP}:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
