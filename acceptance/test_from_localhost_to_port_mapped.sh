6d36fd1481e49796411df2d00a46b3765a1a7fb9
caalpeva <kalpeva@gmail.com>
2022-04-11 20:54:11 +0200

    Rename test file

---
+++ acceptance/test_from_localhost_to_port_mapped.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CALCULATOR_PORT=$(docker-compose -p acceptance port calculator 8080 | cut -d: -f2)
+./gradlew acceptanceTest -Dcalculator.url=http://localhost:${CALCULATOR_PORT}
+#test $(curl localhost:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
