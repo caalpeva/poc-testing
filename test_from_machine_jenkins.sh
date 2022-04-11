6e66abbbd37ff06df57a62e5aed419de5436d24d
caalpeva <kalpeva@gmail.com>
2022-04-11 21:10:23 +0200

    Add network name to get ip address

--- test_from_machine_jenkins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 10
-CALCULATOR_PORT=8080
-CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.Networks.acceptance_default.IPAddress }}" poc_webapp_calculator)
-./gradlew acceptanceTest -Dcalculator.url=http://${CALCULATOR_IP}:${CALCULATOR_PORT}
-#test $(curl ${CALCULATOR_IP}:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
