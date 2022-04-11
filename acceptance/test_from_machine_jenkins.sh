c5d518b7d29668cba223f241cecfefb7ed73e797
caalpeva <kalpeva@gmail.com>
2022-04-11 20:07:53 +0200

    Fix in test.sh

--- acceptance/test_from_machine_jenkins.sh
+++ acceptance/test_from_machine_jenkins.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" calculator)
 sleep 10
 CALCULATOR_PORT=$(docker-compose port poc-webapp-calculator 8080 | cut -d: -f2)
-./gradlew acceptanceTest -Dcalculator.url=http://poc-webapp-calculator:${CALCULATOR_PORT}
+./gradlew acceptanceTest -Dcalculator.url=http://localhost:${CALCULATOR_PORT}
