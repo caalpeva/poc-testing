9f8731e6eb590b3ee8fefcaa282b912726ef97fb
caalpeva <kalpeva@gmail.com>
2022-04-11 20:55:58 +0200

    Rename test file

--- acceptance/test_from_machine_jenkins.sh
+++ acceptance/test_from_machine_jenkins.sh
@@ -2,6 +2,6 @@
 
 sleep 10
 CALCULATOR_PORT=8080
-CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.Networks.acceptance_default.IPAddress }}" calculator)
+CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.Networks.acceptance_default.IPAddress }}" poc_webapp_calculator)
 ./gradlew acceptanceTest -Dcalculator.url=http://${CALCULATOR_IP}:${CALCULATOR_PORT}
 #test $(curl ${CALCULATOR_IP}:${CALCULATOR_PORT}/sum?a=1\&b=2) -eq 3
